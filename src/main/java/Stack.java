d64e53ece22dc2d31668af59275f1b5541f04d26
Reginald Osborn Ekow Yeboah <reoyeboah@st.ug.edu.gh>
2026-08-19 14:49:58 +0000

    Added Stack.java file

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -1,46 +1,83 @@
+import java.util.NoSuchElementException;
+
 public class Stack<T> {
 
+    /** Internal singly-linked node. Each node only needs to know what's below it. */
     private static class Node<T> {
-        T data;
+        final T value;
         Node<T> next;
 
-        Node(T data) {
-            this.data = data;
+        Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
         }
     }
 
     private Node<T> top;
     private int size;
 
-    public void push(T item) {
-        Node<T> newNode = new Node<>(item);
-        newNode.next = top;
-        top = newNode;
-        size++;
-    }
-
-    public T pop() {
-        if (isEmpty()) {
-            throw new IllegalStateException("Stack is empty");
-        }
-        T item = top.data;
-        top = top.next;
-        size--;
-        return item;
-    }
-
-    public T peek() {
-        if (isEmpty()) {
-            throw new IllegalStateException("Stack is empty");
-        }
-        return top.data;
+    public Stack() {
+        this.top = null;
+        this.size = 0;
     }
 
     public boolean isEmpty() {
-        return size == 0;
+        return top == null;
     }
 
     public int size() {
         return size;
     }
+
+    /** Pushes a value onto the top of the stack. O(1). */
+    public void push(T value) {
+        top = new Node<>(value, top);
+        size++;
+    }
+
+   
+    public T pop() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("pop() called on empty stack");
+        }
+        T value = top.value;
+        top = top.next;
+        size--;
+        return value;
+    }
+
+    
+    public T peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("peek() called on empty stack");
+        }
+        return top.value;
+    }
+
+    
+    public void printState(String label) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(label).append(" | top -> [");
+        Node<T> current = top;
+        while (current != null) {
+            sb.append(current.value);
+            if (current.next != null) sb.append(", ");
+            current = current.next;
+        }
+        sb.append("] <- bottom  (size=").append(size).append(")");
+        System.out.println(sb);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        Node<T> current = top;
+        while (current != null) {
+            sb.append(current.value);
+            if (current.next != null) sb.append(", ");
+            current = current.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }   
 }
